7fa9b2f9d202f8aae36b45fcc9e06e879db9d77d
Krzysztof Piaskowy <krzychu2956@gmail.com>
2022-04-24 01:19:07 +0200

    Generate plots

--- sandbox/src/data_compressor/compressor.py
+++ sandbox/src/data_compressor/compressor.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from src.metrics import Metric
 from src.data_type import Measurement
 from typing import List
+import os
 
 class Compressor:
 
@@ -25,20 +26,34 @@ class Compressor:
     self.original_data = []
     self.compressed_data = []
 
-  def vizualize(self, show_compressed: bool = True, show_interpolation: bool = False):
+  def vizualize(self, show_compressed: bool = True, show_interpolation: bool = False, config: any = {}):
     x_original = [measurement.timestamp for measurement in self.original_data]
     y_original = [measurement.value for measurement in self.original_data]
-    plt.figure(dpi=150)
+    plt.figure(dpi=100)
     if show_interpolation:
-        plt.plot(x_original, y_original, 'b')
-    plt.plot(x_original, y_original, 'bo')
+      plt.plot(x_original, y_original, 'b')
+    plt.plot(x_original, y_original, 'bo', label='Original data')
     if show_compressed:
       x_compressed = [measurement.timestamp for measurement in self.compressed_data]
       y_compressed = [measurement.value for measurement in self.compressed_data]
       if show_interpolation:
         plt.plot(x_compressed, y_compressed, 'r')
-      plt.plot(x_compressed, y_compressed, 'ro')
+      plt.plot(x_compressed, y_compressed, 'ro', label='Compressed data')
+    if 'title' in config:
+      plt.title(config['title'])
+      plt.xlabel("time [ms]")
+      plt.ylabel("measurement value [x]")
+      plt.legend(loc="upper right")
     plt.grid()
+    if 'dir' in config:
+      path = 'images/' + config['dir'] + '/'
+      if 'fileName' in config:
+        full_path = 'images/' + config['dir'] + '/' + config['fileName'] + '.png'
+      else:
+        full_path = 'images/' + config['dir'] + '/' + config['title'] + '.png'
+      if not os.path.exists(path):
+        os.makedirs(path)
+      plt.savefig(full_path)
     plt.show()
     return self
 
