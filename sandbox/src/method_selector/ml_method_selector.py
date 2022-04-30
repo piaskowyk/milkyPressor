ad145ced8e5b21be9cc9fdfb6308eeb4dece919c
Krzysztof Piaskowy <krzychu2956@gmail.com>
2022-04-30 23:29:01 +0200

    ML method

--- sandbox/src/method_selector/ml_method_selector.py
+++ sandbox/src/method_selector/ml_method_selector.py
@@ -1,3 +1,42 @@
+from metric import SingleMetric
+from data_type import Measurement
+from typing import List
+from method_selector import ClassicMethodSelector, MlMethodSelector
+from measurement_provider import MeasurementProvider
+from method_selector import ClassicMethodSelector
+from sklearn import tree
+
 class MlMethodSelector:
-  pass
-# todo
+  def __init__(self) -> None:
+    self.measurement_provider: MeasurementProvider = MeasurementProvider()
+    self.measurements_set: List[List[Measurement]] = []
+    self.single_metrics_container = SingleMetric()
+
+  def set_measurements(self, measurements: List[List[Measurement]] = None):
+    if measurements == None:
+      self.measurements_set = self.measurement_provider.get_random1()
+    else:
+      self.measurements_set = measurements
+
+  def set_metrics(self):
+    pass # todo
+
+  def train(self):
+    dataset = self._prepare_dataset()
+    X_train = [data for data, _ in dataset]
+    y_train = [label for _, label in dataset]
+    X_test = [data for data, _ in dataset[:2]]
+    y_test = [label for _, label in dataset[:2]]
+    classifier = tree.DecisionTreeClassifier()
+    classifier = classifier.fit(X_train, y_train)
+    score = classifier.score(X_test, y_test)
+    return classifier, score
+
+  def _prepare_dataset(self):
+    dataset = []
+    for measurements in self.measurements_set:
+      classic_method_selector = ClassicMethodSelector()
+      best_method_name = classic_method_selector.get_best(measurements)
+      input = self.single_metrics_container.compute_all(measurements)
+      dataset.append([input, best_method_name])
+    return dataset
--- sandbox/src/model_generator.py
+++ sandbox/src/model_generator.py
@@ -1,51 +1,22 @@
-# todo:
-# - wybierz metryki
-
-from metric import SingleMetric
 from data_type import Measurement
 from typing import List
-from method_selector import ClassicMethodSelector, MlMethodSelector
-from measurement_provider import MeasurementProvider
+from method_selector import MlMethodSelector
 
-from sklearn import tree
 from fogml.generators import GeneratorFactory
 
 class ModelGenerator:
   def __init__(self) -> None:
-    self.measurement_provider: MeasurementProvider = MeasurementProvider()
-    self.measurements_set: List[Measurement] = []
-    self.ml_method_selector: ClassicMethodSelector = MlMethodSelector()
-    self.single_metrics_container = SingleMetric()
+    self.ml_method_selector: MlMethodSelector = MlMethodSelector()
 
   def set_measurements(self, measurements: List[Measurement] = None):
-    if measurements == None:
-      self.measurements_set = self.measurement_provider.get_random1()
-    else:
-      self.measurements_set = measurements
+    self.ml_method_selector.set_measurements(measurements)
+
+  def set_metrics(self):
+    pass # todo
 
   def build(self):
-    dataset = self._prepare_dataset()
-    X_train = [data for data, _ in dataset]
-    y_train = [label for _, label in dataset]
-
-    X_test = [data for data, _ in dataset[:2]]
-    y_test = [label for _, label in dataset[:2]]
-
-    classifier = tree.DecisionTreeClassifier()
-    classifier = classifier.fit(X_train, y_train)
-
-    score = classifier.score(X_test, y_test)
-
+    classifier, score = self.ml_method_selector.train()
     factory = GeneratorFactory()
     generator = factory.get_generator(classifier)
     generator.generate(fname='output.c')
     return score
-  
-  def _prepare_dataset(self):
-    dataset = []
-    for measurements in self.measurements_set:
-      classic_method_selector = ClassicMethodSelector()
-      best_method_name = classic_method_selector.get_best(measurements)
-      input = self.single_metrics_container.compute_all(measurements)
-      dataset.append([input, best_method_name])
-    return dataset
